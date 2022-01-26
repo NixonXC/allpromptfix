417b4c9e00c5a7107d97201f76d6033f9d46ec85
NixonXC <81410474+NixonXC@users.noreply.github.com>
2022-01-26 10:08:00 -0800

    Add files via upload

--- OS/script.js
+++ OS/script.js
@@ -2,15 +2,13 @@ let taskbar = document.getElementsByClassName("mtask")[0]
 let startmenu = document.getElementsByClassName("startmenu")[0]
 let fold = document.getElementsByClassName("fold")[0]
 let xp = document.getElementsByClassName("xp")[0]
-let terminal = document.getElementsByClassName("term")[0]
-let termi = document.getElementsByClassName("termi")[0]
 
 taskbar.addEventListener("click", ()=>{
-    if(startmenu.style.bottom == "50px"){
+    if(startmenu.style.bottom == "31px"){
         startmenu.style.bottom = "-655px"
     }
     else{
-        startmenu.style.bottom = "50px"
+        startmenu.style.bottom = "31px"
     }
 });
 fold.addEventListener("click", ()=>{
@@ -19,22 +17,6 @@ fold.addEventListener("click", ()=>{
     }
     else{
         xp.style.bottom = "70px"
-    }
-});
-terminal.addEventListener("click", ()=>{
-    if(termi.style.bottom == "70px"){
-        termi.style.bottom = "-655px"
-    }
-    else{
-        termi.style.bottom = "70px"
-    }
-});
-termi.addEventListener("click", ()=>{
-    if(termi.style.bottom == "70px"){
-        termi.style.bottom = "-655px"
-    }
-    else{
-        termi.style.bottom = "70px"
     }
 });
 xp.addEventListener("click", ()=>{
